235e5c4ab54b7a822674e5132b5892984cc9ba2e
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2020-05-27 18:49:09 -0300

    Checagem do bd, listagem de arquivos com problema, atualização de status

--- mail.py
+++ mail.py
@@ -3,6 +3,8 @@ from email.mime.multipart import MIMEMultipart
 import smtplib
 import jinja2
 import os
+import sqlite3
+import datetime
 
 def sendMailHTML(server,port,user,password,to,subject,html):
     msg = MIMEMultipart()
@@ -24,3 +26,36 @@ def renderHTML(fileName,content):
     output = template.render(content)
 
     return output
+
+def checkDB(DBPath):
+    files = []
+
+    conn = sqlite3.connect(DBPath)
+
+    cur = conn.cursor()
+    curUp = conn.cursor()
+    curSt = conn.cursor()
+
+    cur.execute("SELECT * FROM files")
+
+    for file in cur.fetchall():
+        curUp.execute("SELECT measure_time, last_update_in_s FROM updates WHERE file_id = :file ORDER BY id DESC LIMIT 1",{'file':file[0]})
+        result = curUp.fetchone()
+
+        time = datetime.datetime.utcnow()
+        measure_time = datetime.datetime.strptime(result[0],'%Y-%m-%dT%H:%M:%S')
+
+        no_update_time = (time - measure_time).seconds + result[1]
+
+        if file[3] == 0 and no_update_time >= 3600:
+            curSt.execute("UPDATE files SET status = -1 WHERE id = :file",{'file':file[0]})
+            files.append(file[1]+'-'+file[2])
+        elif file[3] == -1 and no_update_time < 3600:
+            curSt.execute("UPDATE files SET status = 0 WHERE id = :file",{'file':file[0]})
+
+    conn.commit()
+    conn.close()
+
+    return files
+
+print(checkDB('db.db'))
